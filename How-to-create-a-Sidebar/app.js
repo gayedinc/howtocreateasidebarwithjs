8ba3960410de626abc676c0ec57070a366902abe
Gaye Dinç <58970025+gayedinc@users.noreply.github.com>
2024-04-27 18:39:02 +0300

    Update app.js

--- How-to-create-a-Sidebar/app.js
+++ How-to-create-a-Sidebar/app.js
@@ -17,20 +17,3 @@ closeBtn.addEventListener("click", function () {
     sidebar.classList.remove('show-sidebar');
 
 })
-
-/* AÇIKLAMALAR
-
-classList, bir HTML elementinin sınıf özelliklerini manipüle etmek için kullanılan bir DOM özelliğidir. classList nesnesi, bir elementin sınıf listesini temsil eder ve bu liste üzerinde işlemler gerçekleştirmenizi sağlar.
-
-classList nesnesinin temel metotları şunlardır:
-
-1- add(className): Belirtilen sınıf adını elementin sınıf listesine ekler.
-2- remove(className): Belirtilen sınıf adını elementin sınıf listesinden kaldırır.
-3- toggle(className): Belirtilen sınıf adını elementin sınıf listesinde varsa kaldırır, yoksa ekler.
-4- contains(className): Belirtilen sınıf adının elementin sınıf listesinde olup olmadığını kontrol eder.
-5- item(index): Belirtilen sıradaki sınıf adını döndürür.
-6- length: Elementin sınıf listesinde bulunan sınıf sayısını döndürür.
-Bu metotları kullanarak, JavaScript ile HTML elementlerinin sınıf listelerini dinamik olarak yönetebilirsiniz. Örneğin, bir elemente bir sınıf eklemek veya kaldırmak için add ve remove metotlarını kullanabilirsiniz. 
-toggle metoduyla ise sınıfın varlığını kontrol edebilir ve varsa kaldırabilir, yoksa ekleyebilirsiniz.
-
- */
